dddc2149a617c7343122b0b2ffdf8b2ac8c4e9e7
tanav2202 <20U03037@iiitbhopal.ac.in>
2021-06-19 04:31:39 +0530

    trying to connect mysql database

--- ContactForm/contact.php
+++ ContactForm/contact.php
@@ -1,12 +1,18 @@
 <?php
 
+$db = new mysqli ("localhost" ,"root", "" , "testing");
+if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+  }
+
 if (isset($_POST['submit'])) {
     $name = trim($_POST['name']);
     $phone = trim($_POST['phone-number']);
     $subject = trim($_POST['subject']);
     $message = trim($_POST['message']);
 
-    header("Location: contacthome.php?mailsent");
+    header("Location: contacthome.php?data_entered");
   
 }
 
