2751714afb6989e47a9122daf0628e8a4cf17ca7
tanav2202 <20U03037@iiitbhopal.ac.in>
2021-06-19 02:12:29 +0530

    contact form added

---
+++ ContactForm/contact.php
@@ -0,0 +1,11 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = trim($_POST['name']);
+    $phone = trim($_POST['phone-number']);
+    $subject = trim($_POST['subject']);
+    $message = trim($_POST['message']);
+
+}
+
+?>
---
+++ ContactForm/contacthome.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div>
+        <h1>IIIT Bhopal Student platform</h1>
+        <p>Please fill in the fields</p>
+
+        <form action="contact.php" method="post">
+            <input type="text" name="name" placeholder="full name">
+            <input type="text" name="phone-number" placeholder="phone number">
+            <input type="text" name="subject" placeholder="scholar number">
+            <textarea name="message" placeholder="Other Details"></textarea>
+            <button type="submit" name="submit">Enter Info Into Data</button>
+        </form>
+    </div>
+</body>
+
+</html>
